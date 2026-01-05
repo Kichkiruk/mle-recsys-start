23e6f79bc282ba84ba9122247e6dc12514a7f509
Kichkiruk <kichrish@gmail.com>
2026-01-05 23:51:13 +0000

    add recommendation_service

---
+++ recommendation_service.py
@@ -0,0 +1,204 @@
+# uvicorn recommendation_service:app
+import logging 
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+import pandas as pd
+import requests
+
+logger = logging.getLogger("uvicorn.error")
+recommendations_url = "http://127.0.0.1:8000"
+features_store_url = "http://127.0.0.1:8010"
+events_store_url = "http://127.0.0.1:8020" 
+
+# Определяем класс Recommendations
+class Recommendations:
+    def __init__(self):
+        self._recs = {"personal": None, "default": None}
+        self._stats = {
+            "request_personal_count": 0,
+            "request_default_count": 0
+        }
+
+    def load(self, type, path, **kwargs):
+        """
+        Загружает рекомендации из файла
+        """
+        logger.info(f"Loading recommendations, type: {type}")
+        self._recs[type] = pd.read_parquet(path, **kwargs)
+        if type == "personal":
+            self._recs[type] = self._recs[type].set_index("user_id")
+        logger.info(f"Loaded")
+
+    def get(self, user_id: int, k: int = 100):
+        """
+        Возвращает список рекомендаций для пользователя
+        """
+        try:
+            recs = self._recs["personal"].loc[user_id]
+            recs = recs["item_id"].to_list()[:k]
+            self._stats["request_personal_count"] += 1
+        except KeyError:
+            recs = self._recs["default"]
+            recs = recs["item_id"].to_list()[:k]
+            self._stats["request_default_count"] += 1
+        except:
+            logger.error(f"No recommendations found")
+            recs = []
+
+        return recs
+
+    def stats(self):
+        logger.info("Stats for recommendations")
+        for name, value in self._stats.items():
+            logger.info(f"{name:<30} {value} ")
+
+# Создаем экземпляр класса как глобальную переменную
+rec_store = Recommendations()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Код ниже (до yield) выполнится только один раз при запуске сервиса
+    logger.info("Starting")
+    
+    # Загружаем персональные рекомендации
+    rec_store.load(
+        "personal",
+        "final_recommendations_feat.parquet",
+        columns=["user_id", "item_id", "rank"],
+    )
+    
+    # Загружаем рекомендации по умолчанию
+    rec_store.load(
+        "default",
+        "top_recs.parquet",
+        columns=["item_id", "rank"],
+    )
+    
+    logger.info("Recommendations loaded successfully")
+    
+    yield
+    
+    # Этот код выполнится только один раз при остановке сервиса
+    # Выводим статистику использования
+    if rec_store:
+        rec_store.stats()
+    
+    logger.info("Stopping")
+
+# Создаём приложение FastAPI
+app = FastAPI(title="recommendations", lifespan=lifespan)
+
+@app.post("/recommendations_offline")
+async def recommendations_offline(user_id: int, k: int = 100):
+    """
+    Возвращает список офлайн-рекомендаций длиной k для пользователя user_id
+    """  
+    recs = rec_store.get(user_id, k)
+    
+    return {"recs": recs}
+
+def dedup_ids(ids):
+    """
+    Дедублицирует список идентификаторов, оставляя только первое вхождение
+    """
+    seen = set()
+    ids = [id for id in ids if not (id in seen or seen.add(id))]
+
+    return ids
+
+@app.post("/recommendations_online")
+async def recommendations_online(user_id: int, k: int = 100):
+    """
+    Возвращает список онлайн-рекомендаций длиной k для пользователя user_id
+    """
+
+    headers = {"Content-type": "application/json", "Accept": "text/plain"}
+
+    # получаем список последних событий пользователя, возьмём три последних
+    params = {"user_id": user_id, "k": 3}
+    resp = requests.post(events_store_url + "/get", headers=headers, params=params)
+    events = resp.json()
+    events = events["events"]
+
+    # получаем список айтемов, похожих на последние три, с которыми взаимодействовал пользователь
+    items = []
+    scores = []
+    for item_id in events:
+        # для каждого item_id получаем список похожих в item_similar_items
+        params = {"item_id": item_id, "k": k}
+        resp = requests.post(features_store_url + "/similar_items", headers=headers, params=params)
+        item_similar_items = resp.json()
+        items += item_similar_items["item_id_2"]
+        scores += item_similar_items["score"]
+    # сортируем похожие объекты по scores в убывающем порядке
+    # для старта это приемлемый подход
+    combined = list(zip(items, scores))
+    combined = sorted(combined, key=lambda x: x[1], reverse=True)
+    combined = [item for item, _ in combined]
+
+    # удаляем дубликаты, чтобы не выдавать одинаковые рекомендации
+    recs = dedup_ids(combined)
+
+    return {"recs": recs[:k]}
+
+@app.post("/recommendations")
+async def recommendations(user_id: int, k: int = 100):
+    """
+    Возвращает список рекомендаций длиной k для пользователя user_id
+    """
+
+    recs_offline = await recommendations_offline(user_id, k)
+    recs_online = await recommendations_online(user_id, k)
+
+    recs_offline = recs_offline["recs"]
+    recs_online = recs_online["recs"]
+
+    recs_blended = []
+
+    min_length = min(len(recs_offline), len(recs_online))
+    # чередуем элементы из списков, пока позволяет минимальная длина
+    for i in range(min_length):
+        recs_blended.append(recs_offline[i])
+        recs_blended.append(recs_online[i])
+
+    # добавляем оставшиеся элементы в конец
+    if len(recs_offline) > min_length:
+        recs_blended.extend(recs_offline[min_length:])
+    if len(recs_online) > min_length:
+        recs_blended.extend(recs_online[min_length:])
+
+    # удаляем дубликаты
+    recs_blended = dedup_ids(recs_blended)
+    
+        # оставляем только первые k рекомендаций
+    recs_blended = recs_blended[:k]
+
+    return {"recs": recs_blended}
+
+
+# @app.post("/recommendations_online")
+# async def recommendations_online(user_id: int, k: int = 100):
+#     """
+#     Возвращает список онлайн-рекомендаций длиной k для пользователя user_id
+#     """
+
+#     headers = {"Content-type": "application/json", "Accept": "text/plain"}
+
+#     # получаем последнее событие пользователя
+#     params = {"user_id": user_id, "k": 1}
+#     resp = requests.post(events_store_url + "/get", headers=headers, params=params)
+#     events = resp.json()
+#     events = events["events"]
+
+#     # получаем список похожих объектов
+#     if len(events) > 0:
+#         item_id = events[0]
+#         params = {"item_id": item_id, "k": k}
+#         resp = requests.post(features_store_url + "/similar_items", headers=headers, params=params)
+#         similar_data = resp.json()
+#         item_similar_items = similar_data.get("item_id_2", [])
+#         recs = item_similar_items[:k]
+#     else:
+#         recs = []
+
+#     return {"recs": recs}
